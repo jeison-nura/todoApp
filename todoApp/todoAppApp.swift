15d01a0f6af383a0992674e707984dd2de943f7c
jeison <jeisonnoc@gmail.com>
2023-03-19 17:18:46 -0500

    Initial Commit

---
+++ todoApp/todoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppApp.swift
+//  todoApp
+//
+//  Created by Jeison Sierra on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct todoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
